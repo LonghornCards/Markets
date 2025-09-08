10b9fd2676eaa56a6256d849bf2b98fc870155d7
LonghornCards <longhorncardsatx@gmail.com>
2025-09-08 10:06:09 -0500

    clean up

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// --- NEW: Amplify setup ---
-import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
-// --------------------------
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
